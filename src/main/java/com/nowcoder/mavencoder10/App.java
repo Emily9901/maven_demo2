1d8cac856249c18634d69c800a45fd52a401bc42
Emily9901 <mf1632563012@163.com>
2023-06-13 10:10:05 +0800

    'Test1'

---
+++ src/main/java/com/nowcoder/mavencoder10/App.java
@@ -0,0 +1,13 @@
+package com.nowcoder.mavencoder10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
